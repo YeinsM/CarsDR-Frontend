45fc27a8759d8ee1676c08c3fd88e64dcf1506a2
YeinsM <wildermancera1@gmail.com>
2025-04-05 00:05:26 -0400

    test commit

--- app/(main)/components/ApplicantInfo.tsx
+++ app/(main)/components/ApplicantInfo.tsx
@@ -1,6 +1,7 @@
 export default function Example() {
     return (
       <div className="bg-gray-50 py-24 sm:py-32">
+        Test
         <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
           <h2 className="text-center text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
           <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
